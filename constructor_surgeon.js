e30eabbc799dea017aa91173f8b5eff132b9c637
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-08-24 11:59:20 +0930

    file on creating class and instance

--- constructor_surgeon.js
+++ constructor_surgeon.js
@@ -1,8 +1,13 @@
+// create a class- capital letter
 class Surgeon {
-    constructor(name, department) {
-      this.name = name;
+    constructor(name, department) { //constructor (parameters)
+      this.name = name; //this. -like self in Python - we say that for this constructor we take the name which is entered as a parameter
       this.department = department;
+    }
+  }
   
-  }
-  }
+  //creating an instance of class Surgeon - with other parameters
+  surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular")
+  // creating an instance of class Surgeon - with other parameters
+  surgeonJackson = new Surgeon ('Ruth Jackson', 'Orthopedics')
   
